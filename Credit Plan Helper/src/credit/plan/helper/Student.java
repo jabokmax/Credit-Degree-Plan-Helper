f78350e63466a5d96e41443767333e7a20ece16f
Phuwit.n <jabokmax@outlook.com>
2017-04-29 19:30:50 +0700

    Student object finish

--- Credit Plan Helper/src/credit/plan/helper/Student.java
+++ Credit Plan Helper/src/credit/plan/helper/Student.java
@@ -10,5 +10,36 @@ package credit.plan.helper;
  * @author Phuwit
  */
 public class Student {
+    private String name;
+    private String department;
+    private String school;
+    private String majorElective;
+    private String minor;
+    
+    /**
+     * #init student object.
+     * @param name 
+     * @param department
+     * @param school
+     * @param majorElective
+     * @param minor 
+     */
+    public void Student(String name, String department, String school,
+            String majorElective, String minor){
+        
+        this.department = department;
+        this.name = name;
+        this.school = school;
+        this.majorElective = majorElective;
+        this.minor = minor;
+        
+    }
+    /**
+     * 
+     * @param name for set a new name.
+     */
+    public void setName(String name){
+        this.name = name;
+    }
     
 }
